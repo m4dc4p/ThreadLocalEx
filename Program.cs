e8bbab19ca95f6f039dbbac456acc5e48fee0153
Justin Bailey <jgbailey@gmail.com>
2011-11-10 14:53:32 -0800

    No op test.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System;
-using System.Runtime.Serialization;
-using System.Runtime.InteropServices;
-using System.Security.Permissions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,7 +12,7 @@ namespace ThreadLocalEx
             int i = 0, j = 0;
             string s = args.Length > 0 ? "1" : "0";
             var tl = new ThreadLocal<string>();
-            var tlx = new ThreadLocalEx<string>();
+            var tlx = new ThreadLocal<string>();
 
             int x = 0, y = 0;
 
@@ -155,124 +148,4 @@ namespace ThreadLocalEx
             return string.Format("[ThreadLocalEx: IsValueCreated={0}, Value={1}]", IsValueCreated, Value);
         }
     }
-
-    //[HostProtection (SecurityAction.LinkDemand, Synchronization = true, ExternalThreading = true)]
-    //public class ThreadLocal<T> : IDisposable
-    //{
-    //    readonly Func<T> valueFactory;
-    //    LocalDataStoreSlot localStore;
-    //    Exception cachedException;
-		
-    //    class DataSlotWrapper
-    //    {
-    //        public bool Creating;
-    //        public bool Init;
-    //        public Func<T> Getter;
-    //    }
-		
-    //    public ThreadLocal () : this (LazyInitializer.GetDefaultValueFactory<T>)
-    //    {
-    //    }
-
-    //    public ThreadLocal (Func<T> valueFactory)
-    //    {
-    //        if (valueFactory == null)
-    //            throw new ArgumentNullException ("valueFactory");
-			
-    //        localStore = Thread.AllocateDataSlot ();
-    //        this.valueFactory = valueFactory;
-    //    }
-		
-    //    public void Dispose ()
-    //    {
-    //        Dispose (true);
-    //    }
-		
-    //    protected virtual void Dispose (bool disposing)
-    //    {
-			
-    //    }
-		
-    //    public bool IsValueCreated {
-    //        get {
-    //            ThrowIfNeeded ();
-    //            return IsInitializedThreadLocal ();
-    //        }
-    //    }
-
-    //    [System.Diagnostics.DebuggerBrowsableAttribute (System.Diagnostics.DebuggerBrowsableState.Never)]
-    //    public T Value {
-    //        get {
-    //            ThrowIfNeeded ();
-    //            return GetValueThreadLocal ();
-    //        }
-    //        set {
-    //            ThrowIfNeeded ();
-
-    //            DataSlotWrapper w = GetWrapper ();
-    //            w.Init = true;
-    //            w.Getter = () => value;
-    //        }
-    //    }
-		
-    //    public override string ToString ()
-    //    {
-    //        return string.Format ("[ThreadLocal: IsValueCreated={0}, Value={1}]", IsValueCreated, Value);
-    //    }
-		
-    //    T GetValueThreadLocal ()
-    //    {
-    //        DataSlotWrapper myWrapper = GetWrapper ();
-    //        if (myWrapper.Creating)
-    //            throw new InvalidOperationException ("The initialization function attempted to reference Value recursively");
-
-    //        return myWrapper.Getter ();
-    //    }
-		
-    //    bool IsInitializedThreadLocal ()
-    //    {
-    //        DataSlotWrapper myWrapper = GetWrapper ();
-
-    //        return myWrapper.Init;
-    //    }
-
-    //    DataSlotWrapper GetWrapper ()
-    //    {
-    //        DataSlotWrapper myWrapper = (DataSlotWrapper)Thread.GetData (localStore);
-    //        if (myWrapper == null) {
-    //            myWrapper = DataSlotCreator ();
-    //            Thread.SetData (localStore, myWrapper);
-    //        }
-
-    //        return myWrapper;
-    //    }
-
-    //    void ThrowIfNeeded ()
-    //    {
-    //        if (cachedException != null)
-    //            throw cachedException;
-    //    }
-
-    //    DataSlotWrapper DataSlotCreator ()
-    //    {
-    //        DataSlotWrapper wrapper = new DataSlotWrapper ();
-    //        Func<T> valSelector = valueFactory;
-	
-    //        wrapper.Getter = delegate {
-    //            wrapper.Creating = true;
-    //            try {
-    //                T val = valSelector ();
-    //                wrapper.Creating = false;
-    //                wrapper.Init = true;
-    //                wrapper.Getter = () => val;
-    //                return val;
-    //            } catch (Exception e) {
-    //                cachedException = e;
-    //                throw e;
-    //            }
-    //        };
-			
-    //        return wrapper;
-    //    }
-    //}
 }
